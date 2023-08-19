cd38cbcf95f6eafbde0d94fc9eb1a3f71afa1a86
p69172002 <142336369+p69172002@users.noreply.github.com>
2023-08-19 21:35:51 +0200

    Update exec.sh

--- exec.sh
+++ exec.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-# ! Make sure to change the username after /home/ !
+# Make sure to change the path name after /home/ to your username
 cd /home/jan/code_dmdreader/
 ./dmdreader serumstuff2/tron.json
